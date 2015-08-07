84af7adbb0e4415354d46a8795cfcabf6abfe494
Seulgi Kim <dev@seulgi.kim>
2015-08-07 12:34:00 +0900

    Add get_sysroot function.
    This is copied from rusti.
    https://github.com/murarth/rusti/blob/dd10c4c74472657cb75c48e2972d8ee3bc0fd8b5/src/rusti/exec.rs#L240
    
    This function is needed when use rustc compiler, because rustc finds standard
    library based on the executable path.
    This function returns the parent of rustc file by `rustc --print sysroot`.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 #![feature(rustc_private)]
 
+#[macro_use]
+extern crate log;
+
 extern crate rustc;
+
+use std::path::PathBuf;
+use std::process::Command;
+use std::str::from_utf8;
 
 fn main() {
     println!("Hello, world!");
+}
+
+/// Runs `rustc` to ask for its sysroot path.
+fn get_sysroot() -> PathBuf {
+    let rustc = if cfg!(windows) { "rustc.exe" } else { "rustc" };
+
+    let output = match Command::new(rustc).args(&["--print", "sysroot"]).output() {
+        Ok(output) => output.stdout,
+        Err(e) => panic!("failed to run rustc: {}", e),
+    };
+
+    let path = from_utf8(&output)
+        .ok().expect("sysroot is not valid UTF-8").trim_right_matches(
+            |c| c == '\r' || c == '\n');
+
+    debug!("using sysroot: {:?}", path);
+
+    PathBuf::from(path)
 }
